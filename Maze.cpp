8b6c10a7fcb23df5414ce7f9f54f2a273e0978ac
jason <helloworldwang2015@outlook.com>
2025-06-27 14:05:57 -0400

    Refactored with iterative and recursive approaches

--- Maze.cpp
+++ Maze.cpp
@@ -113,7 +113,7 @@ public:
                 {
                 case 0: std::cout << ".."; break;
                 case 1: std::cout << "##"; break;
-                case 2: std::cout << "~~"; break;
+                case 2: std::cout << "oo"; break;
                 }
             }
             std::cout << '\n';
@@ -178,7 +178,7 @@ private:
     Position m_end;
 };
 
-int main(int argc, char* argv[])
+int Iterative_OOP()
 {
     constexpr int kMazeSizeRow { 10 };
     constexpr int kMazeSizeCol { 10 };
@@ -198,7 +198,7 @@ int main(int argc, char* argv[])
     maze.print();
     Position start { 1, 1 };
     Position end { kMazeSizeRow - 2, kMazeSizeCol - 2 };
-    
+
     MazeSolver solver { maze, start, end };
     if (solver.solve())
     {
@@ -212,139 +212,236 @@ int main(int argc, char* argv[])
     return 0;
 }
 
+int getIndex(int row, int col);
 
-// int getIndex(int row, int col);
-//
-// namespace Setting
-// {
-//     constexpr int kMazeSizeRow { 10 };
-//     constexpr int kMazeSizeCol { 10 };
-//
-//     enum Direction { null = 0, east, south, west, north, MAX_DIRECTION };
-//     Direction operator++(Direction& dir)
-//     {
-//         if (dir == Direction::MAX_DIRECTION) return dir;
-//         dir = static_cast<Direction>((dir + 1));
-//         return dir;
-//     }
-//     struct Position
-//     {
-//         int row {};
-//         int col {};
-//         Direction dir {};
-//     };
-//     constexpr Position direction[MAX_DIRECTION]
-//     {
-//         Position { 0, 0 },
-//         Position { 0, 1 },
-//         Position { 1, 0 },
-//         Position { 0, -1 },
-//         Position { -1, 0 }
-//     };
-//     Position operator+(const Position& lhs, const Position& rhs)
-//     {
-//         return Position { lhs.row + rhs.row, lhs.col + rhs.col };
-//     }
-//     bool operator==(const Position& lhs, const Position& rhs)
-//     {
-//         return lhs.row == rhs.row && lhs.col == rhs.col;
-//     }
-//     bool operator!=(const Position& lhs, const Position& rhs)
-//     {
-//         return !(lhs == rhs);
-//     }
-// }
-//
-// int getIndex(int row, int col)
-// {
-//     return row * Setting::kMazeSizeCol + col;
-// }
-//
-// void printMaze(int maze[], int sizeRow, int sizeCol)
-// {
-//     for (int row { 0 }; row < sizeRow; ++row)
-//     {
-//         for (int col { 0 }; col < sizeCol; ++col)
-//         {
-//             switch (maze[getIndex(row, col)])
-//             {
-//                 case 0: std::cout << ".."; break;
-//                 case 1: std::cout << "##"; break;
-//                 case 2: std::cout << "~~"; break;
-//             }
-//         }
-//         std::cout << '\n';
-//     }
-// }
-//
-// int main(int argc, char* argv[])
-// {
-//     int maze[Setting::kMazeSizeRow * Setting::kMazeSizeCol]
-//     {
-//         1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-//         1, 0, 1, 0, 0, 0, 0, 0, 0, 1,
-//         1, 0, 1, 0, 1, 1, 1, 1, 0, 1,
-//         1, 0, 1, 0, 0, 0, 0, 1, 0, 1,
-//         1, 0, 1, 1, 1, 1, 0, 1, 0, 1,
-//         1, 0, 0, 0, 0, 1, 0, 0, 0, 1,
-//         1, 1, 1, 1, 0, 1, 0, 1, 0, 1,
-//         1, 0, 0, 0, 0, 0, 0, 1, 0, 1,
-//         1, 0, 1, 1, 1, 1, 1, 1, 0, 1,
-//         1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-//     };
-//     printMaze(maze, Setting::kMazeSizeRow, Setting::kMazeSizeCol);
-//     Setting::Position start { 1, 1 };
-//     Setting::Position end { Setting::kMazeSizeRow - 2, Setting::kMazeSizeCol - 2 };
-//     auto currentPos { start };
-//
-//     LinkedStack<Setting::Position> path;
-//     Setting::Direction dir { Setting::Direction::east };
-//     while (currentPos != end)
-//     {
-//         // find any legal move by this order:
-//         // east -> south -> west -> north
-//         while (dir < Setting::Direction::MAX_DIRECTION)
-//         {
-//             auto nextPos { currentPos + Setting::direction[dir] };
-//             // found legal move
-//             if (maze[getIndex(nextPos.row, nextPos.col)] == 0)
-//             {
-//                 // record and push the direction first
-//                 currentPos.dir = dir;
-//                 path.push(currentPos);
-//                 // mark the path as moved
-//                 maze[getIndex(currentPos.row, currentPos.col)] = 2;
-//                 // move ahead
-//                 currentPos = nextPos;
-//                 // reset dir
-//                 dir = Setting::Direction::east;
-//                 break;
-//             }
-//             ++dir;
-//         }
-//
-//         // no legal move in the current position
-//         if (dir == Setting::Direction::MAX_DIRECTION)
-//         {
-//             do
-//             {
-//                 if (path.empty())
-//                 {
-//                     std::cout << "no solution!\n";
-//                     return 1;
-//                 }
-//                 currentPos = path.pop().value();
-//                 maze[getIndex(currentPos.row, currentPos.col)] = 0;
-//             } while (currentPos.dir == Setting::Direction::north);
-//             // north is the last direction to check, so
-//             // when dir is north, there will be no more choice
-//             dir = static_cast<Setting::Direction>(currentPos.dir + 1);
-//         }
-//     }
-//     // push and mark the endpoint
-//     path.push(currentPos);
-//     maze[getIndex(currentPos.row, currentPos.col)] = 2;
-//     std::cout << "found solution!\n";
-//     printMaze(maze, Setting::kMazeSizeRow, Setting::kMazeSizeCol);
-//     return 0;
-// }
+namespace Setting
+{
+    constexpr int kMazeSizeRow { 10 };
+    constexpr int kMazeSizeCol { 10 };
+
+    enum Direction { null = 0, east, south, west, north, MAX_DIRECTION };
+    Direction operator++(Direction& dir)
+    {
+        if (dir == Direction::MAX_DIRECTION) return dir;
+        dir = static_cast<Direction>((dir + 1));
+        return dir;
+    }
+    struct Position
+    {
+        int row {};
+        int col {};
+        Direction dir {};
+    };
+    constexpr Position direction[MAX_DIRECTION]
+    {
+        Position { 0, 0 },
+        Position { 0, 1 },
+        Position { 1, 0 },
+        Position { 0, -1 },
+        Position { -1, 0 }
+    };
+    Position operator+(const Position& lhs, const Position& rhs)
+    {
+        return Position { lhs.row + rhs.row, lhs.col + rhs.col };
+    }
+    bool operator==(const Position& lhs, const Position& rhs)
+    {
+        return lhs.row == rhs.row && lhs.col == rhs.col;
+    }
+    bool operator!=(const Position& lhs, const Position& rhs)
+    {
+        return !(lhs == rhs);
+    }
+}
+
+int getIndex(int row, int col)
+{
+    return row * Setting::kMazeSizeCol + col;
+}
+
+void printMaze(int maze[], int sizeRow, int sizeCol)
+{
+    for (int row { 0 }; row < sizeRow; ++row)
+    {
+        for (int col { 0 }; col < sizeCol; ++col)
+        {
+            switch (maze[getIndex(row, col)])
+            {
+                case 0: std::cout << ".."; break;
+                case 1: std::cout << "##"; break;
+                case 2: std::cout << "~~"; break;
+            }
+        }
+        std::cout << '\n';
+    }
+}
+
+int Iterative()
+{
+    int maze[Setting::kMazeSizeRow * Setting::kMazeSizeCol]
+    {
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 1, 0, 1, 1, 1, 1, 0, 1,
+        1, 0, 1, 0, 0, 0, 0, 1, 0, 1,
+        1, 0, 1, 1, 1, 1, 0, 1, 0, 1,
+        1, 0, 0, 0, 0, 1, 0, 0, 0, 1,
+        1, 1, 1, 1, 0, 1, 0, 1, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1, 0, 1,
+        1, 0, 1, 1, 1, 1, 1, 1, 0, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    };
+    printMaze(maze, Setting::kMazeSizeRow, Setting::kMazeSizeCol);
+    Setting::Position start { 1, 1 };
+    Setting::Position end { Setting::kMazeSizeRow - 2, Setting::kMazeSizeCol - 2 };
+    auto currentPos { start };
+
+    LinkedStack<Setting::Position> path;
+    Setting::Direction dir { Setting::Direction::east };
+    while (currentPos != end)
+    {
+        // find any legal move by this order:
+        // east -> south -> west -> north
+        while (dir < Setting::Direction::MAX_DIRECTION)
+        {
+            auto nextPos { currentPos + Setting::direction[dir] };
+            // found legal move
+            if (maze[getIndex(nextPos.row, nextPos.col)] == 0)
+            {
+                // record and push the direction first
+                currentPos.dir = dir;
+                path.push(currentPos);
+                // mark the path as moved
+                maze[getIndex(currentPos.row, currentPos.col)] = 2;
+                // move ahead
+                currentPos = nextPos;
+                // reset dir
+                dir = Setting::Direction::east;
+                break;
+            }
+            ++dir;
+        }
+
+        // no legal move in the current position
+        if (dir == Setting::Direction::MAX_DIRECTION)
+        {
+            do
+            {
+                if (path.empty())
+                {
+                    std::cout << "no solution!\n";
+                    return 1;
+                }
+                currentPos = path.pop().value();
+                maze[getIndex(currentPos.row, currentPos.col)] = 0;
+            } while (currentPos.dir == Setting::Direction::north);
+            // north is the last direction to check, so
+            // when dir is north, there will be no more choice
+            dir = static_cast<Setting::Direction>(currentPos.dir + 1);
+        }
+    }
+    // push and mark the endpoint
+    path.push(currentPos);
+    maze[getIndex(currentPos.row, currentPos.col)] = 2;
+    std::cout << "found solution!\n";
+    printMaze(maze, Setting::kMazeSizeRow, Setting::kMazeSizeCol);
+    return 0;
+}
+
+namespace MazeSetting
+{
+    constexpr int kMazeSizeRow { 10 };
+    constexpr int kMazeSizeCol { 10 };
+
+    int maze[kMazeSizeRow * kMazeSizeCol]
+    {
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 1, 0, 1, 1, 1, 1, 0, 1,
+        1, 0, 1, 0, 0, 0, 0, 1, 0, 1,
+        1, 0, 1, 1, 1, 1, 0, 1, 0, 1,
+        1, 0, 0, 0, 0, 1, 0, 0, 0, 1,
+        1, 1, 1, 1, 0, 1, 0, 1, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1, 0, 1,
+        1, 0, 1, 1, 1, 1, 1, 1, 0, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    };
+
+    struct Position
+    {
+        int row {};
+        int col {};
+    };
+    bool operator==(const Position& lhs, const Position& rhs)
+    {
+        return lhs.row == rhs.row && lhs.col == rhs.col;
+    }
+
+    constexpr Position start { 1, 1 };
+    constexpr Position end { kMazeSizeRow - 2, kMazeSizeCol - 2 };
+
+    void printMaze()
+    {
+        for (int row { 0 }; row < kMazeSizeRow; ++row)
+        {
+            for (int col { 0 }; col < kMazeSizeCol; ++col)
+            {
+                switch (maze[row * kMazeSizeCol + col])
+                {
+                    case 0: std::cout << ".."; break;
+                    case 1: std::cout << "##"; break;
+                    case 2: std::cout << "oo"; break;
+                }
+            }
+            std::cout << std::endl;
+        }
+    }
+}
+
+int getIndex(const MazeSetting::Position& pos)      // overloaded
+{
+    return pos.row * MazeSetting::kMazeSizeCol + pos.col;
+}
+
+bool pathFound(const MazeSetting::Position& currentPos)
+{
+    // current Position is invalid
+    if (currentPos.row < 0 || currentPos.row >= MazeSetting::kMazeSizeRow
+        || currentPos.col < 0 || currentPos.col >= MazeSetting::kMazeSizeCol
+        || MazeSetting::maze[getIndex(currentPos)])     // current Position is inside the wall
+        return false;
+
+    // current Position is valid, then mark the tile
+    MazeSetting::maze[getIndex(currentPos)] = 2;
+
+    // if current Position is already the endpoint, exit
+    if (currentPos == MazeSetting::end)
+        return true;
+
+    // otherwise, do recursive step to find next move
+    if (pathFound(MazeSetting::Position{currentPos.row, currentPos.col + 1})) return true;   // EAST
+    if (pathFound(MazeSetting::Position{currentPos.row + 1, currentPos.col})) return true;   // SOUTH
+    if (pathFound(MazeSetting::Position{currentPos.row, currentPos.col - 1})) return true;   // WEST
+    if (pathFound(MazeSetting::Position{currentPos.row - 1, currentPos.col})) return true;   // NORTH
+
+    // path in any direction is blocked, then
+    // unmark the current Position and return
+    MazeSetting::maze[getIndex(currentPos)] = 0;
+    return false;
+}
+
+int main(int argc, char* argv[])
+{
+    std::cout << "MAZE:\n\n";
+    MazeSetting::printMaze();
+    if (pathFound(MazeSetting::start))
+    {
+        std::cout << "Path found!\n";
+        MazeSetting::printMaze();
+    }
+    else
+    {
+        std::cout << "Path not found!\n";
+    }
+    return 0;
+}
