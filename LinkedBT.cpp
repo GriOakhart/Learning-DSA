71b9dc0bcd09fdcb073bd2dcba3caeffe70c6721
jason <helloworldwang2015@outlook.com>
2025-07-02 11:56:27 -0400

    Fixed a bug where the null nodes are not enqueued

--- LinkedBT.cpp
+++ LinkedBT.cpp
@@ -36,21 +36,24 @@ void LinkedBT<T>::init(const T (&arr)[N])
     while (currentIndex < N)
     {
         auto parent { parentQueue.dequeue().value() };
+        if (!parent) throw std::runtime_error("parent is null");
 
         if (arr[currentIndex])
         {
+            std::cerr << "enqueue " << arr[currentIndex] << " to parentQueue" << std::endl;
             parent->left = new TreeNode { arr[currentIndex] };
-            parentQueue.enqueue(parent->left);
         }
+        parentQueue.enqueue(parent->left);
         ++currentIndex;
 
         if (currentIndex < N)
         {
             if (arr[currentIndex])
             {
+                std::cerr << "enqueue " << arr[currentIndex] << " to parentQueue" << std::endl;
                 parent->right = new TreeNode { arr[currentIndex] };
-                parentQueue.enqueue(parent->right);
             }
+            parentQueue.enqueue(parent->right);
             ++currentIndex;
         }
     }
@@ -58,7 +61,7 @@ void LinkedBT<T>::init(const T (&arr)[N])
 
 int main(int argc, char* argv[])
 {
-    int myArr[16] { 0, 6, 3, 8, 2, 5, 7, 9, 0, 0, 4, 0, 0, 0, 0, 10 };
+    int myArr[17] { 0, 6, 3, 8, 2, 5, 7, 9, 0, 0, 4, 0, 0, 0, 0, 10, 1 };
     LinkedBT<int> tree;
     tree.init(myArr);
     return 0;
